a33e1c42e708da135c9a4ae657b3c48819a0db23
strong__cat <ferrari.roma@yandex.ru>
2019-03-27 15:12:56 +0100

    start

---
+++ sample/Controller.java
@@ -0,0 +1,81 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Controller {
+
+
+
+
+    @FXML
+    private MenuItem new_conn;
+
+    @FXML
+    private Menu menu_item;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label drag;
+
+    @FXML
+    private Button bu1;
+
+
+
+    @FXML
+    void initialize(ActionEvent event) {
+    }
+
+
+
+    @FXML
+    void initialize() {
+
+         new_conn.setOnAction(event -> {
+            String message = "das";
+            int people_result=0;
+            System.out.println(message);
+             ////
+             try {
+                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("connection_form.fxml"));
+                 Parent root1 = (Parent) fxmlLoader.load();
+                 Stage stage = new Stage();
+                 stage.setScene(new Scene(root1));
+                 stage.setTitle("Roma Makeev");
+                 stage.show();
+             }
+             catch(Exception e) {
+                 e.printStackTrace();
+             }
+
+
+
+
+
+
+
+
+
+        });
+
+
+    }
+
+
+
+}
+
---
+++ sample/Main.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main_page.fxml"));
+        primaryStage.setTitle("Roma Makeev");
+        primaryStage.setScene(new Scene(root, 600, 375));
+        primaryStage.show();
+
+        ////
+
+
+    }
+
+    public void pressButton(ActionEvent event) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("connetction_form.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+}
